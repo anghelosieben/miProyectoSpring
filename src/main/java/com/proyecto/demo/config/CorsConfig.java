dec16f3d118f0ada147726d7831e52ff23ea6910
anghelo muñoz lopez <anghelosieben13@gmail.com>
2026-02-04 23:08:39 -0400

    Adicion de cors para angular

---
+++ src/main/java/com/proyecto/demo/config/CorsConfig.java
@@ -0,0 +1,18 @@
+package com.proyecto.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:4200", "http://localhost") // agrega más si necesitas
+                .allowedMethods("*")  // o lista específica: GET, POST, etc.
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
