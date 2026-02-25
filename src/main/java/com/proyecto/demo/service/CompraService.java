c0afb71274461f98900916f3cd9fdd9cd631e0c6
anghelo muñoz lopez <anghelosieben13@gmail.com>
2026-02-25 19:15:19 -0400

    Adicionar metodos para controller https://github.com/anghelosieben/miProyectoSpring/issues/6

--- src/main/java/com/proyecto/demo/service/CompraService.java
+++ src/main/java/com/proyecto/demo/service/CompraService.java
@@ -1,7 +1,21 @@
 package com.proyecto.demo.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import com.proyecto.demo.model.entity.Compra;
 
+/**
+ * @author Anghelo Muñoz Lopez
+ */
 public interface CompraService {
-    public Compra realizarCompra(Compra compra);
+    Compra realizarCompra(Compra compra);
+    List<Compra> findAll();
+    Optional<Compra> findById(Long id);
+    Compra save(Compra compra);
+    void deleteById(Long id);
+    Page<Compra> findAllPageable(Pageable pageable);
 }
